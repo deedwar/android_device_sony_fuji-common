a622a113e8cd58a4a06278157005cc970c111683
Warren <deed821@gmail.com>
2014-03-27 22:31:54 +1100

    remove cm torch overlay, update telephony ril and remove maxlayer
    Change-Id: I15e1e98221bb0d87def002e4b7c1f128af9476df

--- fuji.mk
+++ fuji.mk
@@ -271,8 +271,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.egl.hw=1 \
     debug.sf.hw=1 \
     persist.hwc.mdpcomp.enable=true \
-    debug.composition.type=dyn \
-    debug.mdpcomp.maxlayer=3 \
     debug.mdpcomp.logs=0 \
     debug.egl.recordable.rgba8888=1
 
@@ -282,7 +280,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # More display props - double check these!
 PRODUCT_PROPERTY_OVERRIDES += \
-    dev.pm.dyn_samplingrate=1 \
     debug.hwc.dynThreshold=1.9
 
 # Low Power Audio Decoding
